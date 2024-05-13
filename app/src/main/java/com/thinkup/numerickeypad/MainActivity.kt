2e85f5253dd8b93058f32535842049b4a2377f6a
martinzarzar <martinzarzar@gmail.com>
2024-05-13 19:02:32 -0300

    Add example

--- app/src/main/java/com/thinkup/numerickeypad/MainActivity.kt
+++ app/src/main/java/com/thinkup/numerickeypad/MainActivity.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.thinkup.numeric_keypad.NumericKeypad
 import com.thinkup.numerickeypad.ui.theme.NumericKeypadTheme
 
@@ -37,14 +52,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
+    var textState by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(24.dp),
         verticalArrangement = Arrangement.Center
     ) {
-        NumericKeypad(onNumberClick = {}, onDeleteClick = {})
+        Spacer(modifier = Modifier.weight(1f))
+        InputDigit(value = textState)
+        Spacer(modifier = Modifier.weight(1f))
+        NumericKeypad(
+            onNumberClick = { number ->
+                textState += number
+            },
+            onDeleteClick = {
+                if (textState.isNotEmpty()) {
+                    textState = textState.dropLast(1)
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(32.dp))
     }
+}
+
+@Composable
+private fun InputDigit(value: String){
+    Text(
+        text = value,
+        color = Color.Black,
+        textAlign = TextAlign.End,
+        maxLines = 1,
+        style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.SemiBold),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(14.dp)
+            .background(
+                color = MaterialTheme.colorScheme.tertiary,
+                shape = RoundedCornerShape(8.dp)
+            )
+    )
 }
 
 
